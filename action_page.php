88b22fea16325f588f2c0433a9082055bebf10d3
ValBalle <99769832+ValBalle@users.noreply.github.com>
2022-03-22 16:51:36 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -2,7 +2,9 @@
 <body>
 
 Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Your Iname address is: <?php echo $_POST["Iname"]; ?><br>
+
+
 
 </body>
 </html>
