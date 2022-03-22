af71d05252270eeef4c3dc92046168250cc95161
ValBalle <99769832+ValBalle@users.noreply.github.com>
2022-03-22 17:01:37 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -4,7 +4,8 @@
 Welcome <?php echo $_POST["name"]; ?><br>
 Your Iname address is: <?php echo $_POST["Iname"]; ?><br>
 <?php echo $_POST["Telefono"]; ?><br>
-
+<?php echo $_POST["Correo"]; ?><br>
+<?php echo $_POST["Inscripción"]; ?><br>
 
 
 </body>
